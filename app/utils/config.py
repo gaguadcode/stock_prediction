bd08d9efead0bb604cd22550dcf200307a484d20
gustavo.aguado <gustavo.aguado@accenture.com>
2025-02-19 10:08:20 +0100

    added langsmith config params

--- app/utils/config.py
+++ app/utils/config.py
@@ -3,6 +3,10 @@ class Config(BaseSettings):
     ALPHAVANTAGE_API_KEY: str
     ALPHAVANTAGE_BASE_URL: str
     OUTPUT_CSV: str
+    langsmith_tracing: bool  # Change to correct type (bool instead of str if applicable)
+    langsmith_endpoint: str
+    langsmith_api_key: str
+    langsmith_project: str
 
     class Config:
         env_file = ".env"
